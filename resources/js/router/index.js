33bde515335d544aa9da40e533c1ff5997b5acdf
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-11-18 06:00:39 -0500

    added routes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const HomeComponent = () => import ('../web/Home.vue');
-const LoginComponent = () => import ('../web/auth/Login.vue');
+const HomeComponent = () => import ('../components/Home.vue');
+const LoginComponent = () => import ('../components/auth/Login.vue');
 
 const routes=[
-    { path:'/', name:'home', component:HomeComponent},
-    { path:'/login', name:'login', component:LoginComponent},
+    { path:'/', name:'home', component: HomeComponent},
+    { path:'/login', name:'login', component: LoginComponent},
+    { path:'/rifas', name:'rifas', component: LoginComponent},
+    { path:'/winners', name:'winners', component: LoginComponent},
+    { path:'/about', name:'about', component: LoginComponent},
+    { path:'/contact', name:'contact', component: LoginComponent},
 ];
 
 const router = createRouter({
