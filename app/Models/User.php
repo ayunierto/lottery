47e7331d949eeff9ca3b60d338d2a843a8b318c7
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-11-18 07:44:50 -0500

    logic for users

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,3 +41,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+
